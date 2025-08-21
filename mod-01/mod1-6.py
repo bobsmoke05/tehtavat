ce59ad9aecbcc397cc49bfda53e71693fc7e18a1
Bob <bobhyvakka@gmail.com>
2025-08-21 12:03:15 +0300

    a

---
+++ mod-01/mod1-6.py
@@ -0,0 +1,23 @@
+import random
+
+kolme = 0
+nelja = 0
+
+moi = ""
+hei = ""
+
+while kolme < 3:
+    eka = random.randint(0,9)
+    moi += str(eka)
+    kolme += 1
+
+print(moi)
+
+while nelja < 4:
+    toka = random.randint(1,6)
+    hei += str(toka)
+    nelja += 1
+
+print(hei)
+
+
