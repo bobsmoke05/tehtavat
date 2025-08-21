8e5cf3e077539c7eba3539acffeb3d1c73cef4fc
Bob <bobhyvakka@gmail.com>
2025-08-21 09:05:17 +0300

    a

---
+++ mod-01/mod1-3.py
@@ -0,0 +1,8 @@
+kanta = float(input("Anna suorakulmion kanta: "))
+korkeus = float(input("Anna suorakulmion korkeus: "))
+
+pinta_ala = kanta * korkeus
+piiri = 2 * (kanta + korkeus)
+
+print("Suorakulmion pinta-ala:", pinta_ala)
+print("Suorakulmion piiri:", piiri)
---
+++ mod-01/mod1-4.py
@@ -0,0 +1,8 @@
+eka = int(input("Eka: "))
+toka = int(input("Toka: "))
+kolmas = int(input("Kolmas: "))
+
+avg = eka + toka + kolmas
+print(avg)
+print(eka * toka * kolmas)
+print(avg//3)
