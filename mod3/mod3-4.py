9820edb0c43838be49132800af803d20313ccd33
Bob <bobhyvakka@gmail.com>
2025-08-26 13:38:19 +0300

    a

--- mod3/mod3-4.py
+++ mod3/mod3-4.py
@@ -4,6 +4,8 @@ if vuosi % 4 == 0:
     if vuosi % 100 == 0:
         if vuosi % 400 == 0:
             print("Kyllä on")
+        else:
+            print("Ei ole")
     else:
         print("Kyllä on")
 else:
