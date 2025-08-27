164a6dd488d4cd9de1bdc63850d58fedd47d946e
Bob <bobhyvakka@gmail.com>
2025-08-27 13:00:08 +0300

    a

--- mod3/mod3-3.py
+++ mod3/mod3-3.py
@@ -1,18 +1,18 @@
 sp = input("Kerro sukupuolesi: ")
-hemo = int(input("Anna hemogobliiniarvosi: "))
+hemo = int(input("Anna hemoglobiiniarvosi: "))
 
 if sp == "nainen":
     if hemo < 117:
-        print("Hemogobliiniarvosi on alhainen")
+        print("Hemoglobiiniarvosi on alhainen")
     elif hemo > 175:
-        print("Hemogobliiniarvosi on korkea")
+        print("Hemoglobiiniarvosi on korkea")
     else:
-        print("Hemogobliiniarvosi on normaali")
+        print("Hemoglobiiniarvosi on normaali")
 
 if sp == "mies":
     if hemo < 134:
-        print("Hemogobliiniarvosi on alhainen")
+        print("Hemoglobiiniarvosi on alhainen")
     elif hemo > 195:
-        print("Hemogobliiniarvosi on korkea")
+        print("Hemoglobiiniarvosi on korkea")
     else:
-        print("Hemogobliiniarvosi on normaali")
+        print("Hemoglobiiniarvosi on normaali")
